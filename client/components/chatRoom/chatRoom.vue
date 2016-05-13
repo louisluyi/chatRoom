c371b5a1e4b95a6c7ce48315dc8e88b0119394f3
unknown <lysicily@sina.com>
2016-05-13 18:53:01 +0800

    add some modules

--- client/components/chatRoom/chatRoom.vue
+++ client/components/chatRoom/chatRoom.vue
@@ -1,13 +1,27 @@
 <template>
-    <div class="snapshot">
-        <div class=""></div>
+    <div class="main">
+        <ul class="snatshop-list">
+            <li is="snatshop"></li>
+        </ul>
+        <ul class="chat-content-list">
+            <li is="chat-content"></li>
+        </ul>
     </div>
-    <div class="chat-content"></div>
 </template>
 
 <script>
     import './chatRoom.styl';
-    export {
+    import snatshop from '../snapshot/snapshot.vue';
+    import chatContent from '../chatContent/chatContent.vue';
+    export default {
+        data:function(){
+            return{
 
+            }
+        },
+        components:{
+            snatshop,
+            chatContent
+        }
     }
 </script>
